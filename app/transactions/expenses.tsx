33e1d9b472bc1604c581517934f5b917dec3edc0
Eduardo Falarz <eduardo@pontogestor.com>
2025-04-25 21:13:12 -0300

    Style: Created expenses screen

---
+++ app/transactions/expenses.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import FixedScreen from "../../src/components/screen/FixedScreen";
+
+export default function expenses() {
+  return (
+    <FixedScreen>
+      <Text>expenses</Text>
+    </FixedScreen>
+  );
+}
+
+const styles = StyleSheet.create({});
