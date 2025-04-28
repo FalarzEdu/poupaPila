056e763b1347fc7332af9f9cdab89d412cd77faa
Eduardo Falarz <eduardo@pontogestor.com>
2025-04-27 21:29:31 -0300

    REF: Removing comments

--- src/helpers/CurrencyConversion.ts
+++ src/helpers/CurrencyConversion.ts
@@ -1,6 +1,4 @@
 export function convert(value: number) {
-  // value.toString().
-  // return value.toFixed(2).replace(".", ",");
   return value.toLocaleString("de-DE", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
