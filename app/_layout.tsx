4f2fbb91856defcbfe4cf4ddf08f1f8234a08dd0
Eduardo Falarz <eduardofalarz23@gmail.com>
2025-06-18 19:25:46 -0300

    FEAT: Adding more stacks to navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,19 +7,30 @@ export default function _layout() {
   const { theme } = changeThemeStore();
 
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: theme.background.primary },
-        title: "",
-        headerTintColor: theme.text.colours.normal,
-        headerRight: () => (
-          <Ionicons
-            name="ellipsis-horizontal-outline"
-            size={28}
-            color={theme.text.colours.normal}
-          />
-        ),
-      }}
-    ></Stack>
+    <Stack screenOptions={{
+      headerStyle: { backgroundColor: theme.background.primary },
+      headerTintColor: theme.text.colours.normal,
+      headerRight: () => (
+        <Ionicons
+          name="ellipsis-horizontal-outline"
+          size={28}
+          color={theme.text.colours.normal}
+        />
+      ),
+    }}>
+
+      <Stack.Screen name="index" options={{title: ""}} />
+
+      <Stack.Screen name={"transactions/expenses"} options={{title: ""}} />
+
+      <Stack.Screen name={"transactions/revenues"} options={{title: ""}} />
+
+      <Stack.Screen
+        name="transactions/newExpense"
+        options={{
+          title: "Nova Despesa",
+        }}
+      />
+    </Stack>
   );
 }
